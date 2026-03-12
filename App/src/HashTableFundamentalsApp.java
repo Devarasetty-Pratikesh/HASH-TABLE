bca4fce7c26cb6ead02d23d2402375bb55e9bea5
Pratikesh <pratikeshdevarasetty1315@gmail.com>
2026-03-12 10:17:53 +0530

    Prethikesh Added UC10-MultiLevelCacheSystem

--- App/src/HashTableFundamentalsApp.java
+++ App/src/HashTableFundamentalsApp.java
@@ -1,25 +1,30 @@
-import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+class LRUCache extends LinkedHashMap<String, String> {
+    private final int capacity;
+
+    LRUCache(int capacity) {
+        super(capacity, 0.75f, true);
+        this.capacity = capacity;
+    }
+
+    protected boolean removeEldestEntry(Map.Entry<String, String> eldest) {
+        return size() > capacity;
+    }
+}
 
 public class HashTableFundamentalsApp {
     public static void main(String[] args) {
-        int[] amounts = {500, 300, 200, 700};
-        int target = 500;
+        LRUCache l1 = new LRUCache(2);
+        l1.put("video1", "data1");
+        l1.put("video2", "data2");
 
-        findTwoSum(amounts, target);
-    }
+        System.out.println("L1 Cache: " + l1);
 
-    static void findTwoSum(int[] amounts, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+        l1.get("video1");
+        l1.put("video3", "data3");
 
-        for (int i = 0; i < amounts.length; i++) {
-            int complement = target - amounts[i];
-            if (map.containsKey(complement)) {
-                System.out.println("Pair found: " + complement + " + " + amounts[i] + " = " + target);
-                return;
-            }
-            map.put(amounts[i], i);
-        }
-
-        System.out.println("No pair found");
+        System.out.println("After LRU eviction: " + l1);
     }
 }
