9df7770c742eecad6ed079a399031c8e949b0529
Pratikesh <pratikeshdevarasetty1315@gmail.com>
2026-03-12 10:16:52 +0530

    Prethikesh Added UC9-TwoSumTransactions

--- App/src/HashTableFundamentalsApp.java
+++ App/src/HashTableFundamentalsApp.java
@@ -1,37 +1,25 @@
+import java.util.HashMap;
+
 public class HashTableFundamentalsApp {
-    static String[] parking = new String[10];
-
     public static void main(String[] args) {
-        parkVehicle("ABC123");
-        parkVehicle("XYZ999");
-        parkVehicle("ABC124");
-        exitVehicle("XYZ999");
+        int[] amounts = {500, 300, 200, 700};
+        int target = 500;
+
+        findTwoSum(amounts, target);
     }
 
-    static int hash(String plate) {
-        return Math.abs(plate.hashCode()) % parking.length;
-    }
+    static void findTwoSum(int[] amounts, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-    static void parkVehicle(String plate) {
-        int index = hash(plate);
-        int probes = 0;
-
-        while (parking[index] != null) {
-            index = (index + 1) % parking.length;
-            probes++;
-        }
-
-        parking[index] = plate;
-        System.out.println(plate + " parked at " + index + " with " + probes + " probes");
-    }
-
-    static void exitVehicle(String plate) {
-        for (int i = 0; i < parking.length; i++) {
-            if (plate.equals(parking[i])) {
-                parking[i] = null;
-                System.out.println(plate + " exited from " + i);
+        for (int i = 0; i < amounts.length; i++) {
+            int complement = target - amounts[i];
+            if (map.containsKey(complement)) {
+                System.out.println("Pair found: " + complement + " + " + amounts[i] + " = " + target);
                 return;
             }
+            map.put(amounts[i], i);
         }
+
+        System.out.println("No pair found");
     }
 }
