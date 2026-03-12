297ad39c9639c5bceb4f790e527c0086f03cb439
Pratikesh <pratikeshdevarasetty1315@gmail.com>
2026-03-12 09:50:25 +0530

    Prethikesh Added UC5-RealTimeAnalyticsDashboard

--- App/src/HashTableFundamentalsApp.java
+++ App/src/HashTableFundamentalsApp.java
@@ -2,34 +2,32 @@ import java.util.*;
 
 public class HashTableFundamentalsApp {
     public static void main(String[] args) {
-        String doc1 = "java is easy to learn and java is powerful";
-        String doc2 = "java is easy to use and java is powerful";
+        HashMap<String, Integer> pageViews = new HashMap<>();
+        HashMap<String, HashSet<String>> uniqueVisitors = new HashMap<>();
+        HashMap<String, Integer> trafficSource = new HashMap<>();
 
-        Set<String> ngrams1 = getNGrams(doc1, 3);
-        Set<String> ngrams2 = getNGrams(doc2, 3);
+        processEvent(pageViews, uniqueVisitors, trafficSource, "/home", "u1", "Google");
+        processEvent(pageViews, uniqueVisitors, trafficSource, "/home", "u2", "Facebook");
+        processEvent(pageViews, uniqueVisitors, trafficSource, "/about", "u1", "Google");
 
-        int matches = 0;
-        for (String gram : ngrams1) {
-            if (ngrams2.contains(gram)) {
-                matches++;
-            }
+        System.out.println("Page Views: " + pageViews);
+        System.out.println("Unique Visitors: ");
+        for (String page : uniqueVisitors.keySet()) {
+            System.out.println(page + " -> " + uniqueVisitors.get(page).size());
         }
-
-        double similarity = (double) matches / Math.max(ngrams1.size(), ngrams2.size()) * 100;
-        System.out.println("Similarity: " + similarity + "%");
+        System.out.println("Traffic Sources: " + trafficSource);
     }
 
-    static Set<String> getNGrams(String text, int n) {
-        String[] words = text.split(" ");
-        Set<String> grams = new HashSet<>();
+    static void processEvent(HashMap<String, Integer> pageViews,
+                             HashMap<String, HashSet<String>> uniqueVisitors,
+                             HashMap<String, Integer> trafficSource,
+                             String page, String user, String source) {
 
-        for (int i = 0; i <= words.length - n; i++) {
-            StringBuilder sb = new StringBuilder();
-            for (int j = i; j < i + n; j++) {
-                sb.append(words[j]).append(" ");
-            }
-            grams.add(sb.toString().trim());
-        }
-        return grams;
+        pageViews.put(page, pageViews.getOrDefault(page, 0) + 1);
+
+        uniqueVisitors.putIfAbsent(page, new HashSet<>());
+        uniqueVisitors.get(page).add(user);
+
+        trafficSource.put(source, trafficSource.getOrDefault(source, 0) + 1);
     }
 }
